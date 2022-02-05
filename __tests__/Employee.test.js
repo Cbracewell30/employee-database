0e96650e73efb0c5263c537d50e3fc60c0076fe1
Chris <chrisbracewell35@gmail.com>
2022-02-05 14:40:36 -0500

    adding employee name, id, and email test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,12 +1,26 @@
-const { expect } = require('@jest/globals');
-const { test } = require('picomatch');
-const Player = require('../lib/Employee');
+const Employee = require('../lib/Employee');
 
 
 test('create employee', () =>{
+    const employee = new Employee ();
+    
+    expect(typeof(employee)).toBe("object");
+});
+
+test('Checking employee name', () =>{
     const employee = new Employee ('Chris');
     
     expect(employee.name).toBe('Chris');
-    expect(employee.)
+});
+
+test('Checking Employee ID', () =>{
+    const employee = new Employee ("Chris",123);
+    
+    expect(employee.id).toBe(123);
 })
 
+test('Checking Email', () =>{
+    const employee = new Employee ("Chris",123, "chris@gmail.com");
+    
+    expect(employee.email).toBe("chris@gmail.com");
+})
